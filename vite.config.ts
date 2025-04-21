ec63ed6fda960cf4c586fd247abb9a3112580799
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-04-21 15:37:39 -0300

    feat: update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/teach_me/',
   plugins: [react()],
+  base: '/teach_me/', // <-- aqui é o pulo do gato
 })
